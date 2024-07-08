dbff37d51a7b08691c0d9bffe60c179c93cfc265
Anil Rai <anilchamingrai46@gmail.com>
2024-07-08 22:40:23 +0545

    profile page added

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native'
+import UserIntro from '../../components/Profile/UserIntro'
+import MenuList from '../../components/Profile/MenuList'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={{ 
+     }}>
+      <Text style={{ 
+        fontFamily:'poppins-bold',
+        fontSize:25,
+        paddingLeft:20,
+       }}>Profile</Text>
+       
+       <UserIntro/>
+
+       <MenuList/>
+    </SafeAreaView>
   )
 }
 
---
+++ components/Profile/UserIntro.jsx
@@ -0,0 +1,28 @@
+import { View, Text,Image } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+
+const UserIntro = () => {
+    const {user} = useUser();
+  return (
+    <View style={{ 
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:30
+     }}>
+      <Image
+        source={{ uri:user?.imageUrl }}
+        style={{ 
+            width:100,
+            height:100,
+            borderRadius:99
+         }}
+      />
+      <Text style={{ fontFamily:'poppins-bold',fontSize:20 }}>{user?.fullName}</Text>
+      <Text style={{ fontFamily:'poppins',fontSize:16 }}>{user?.primaryEmailAddress?.emailAddress}</Text>
+    </View>
+  )
+}
+
+export default UserIntro
